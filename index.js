d65585cdbe683a3ba84155bdd7cd55b78847a85d
Francesca Hamm <francesca.c.hamm@gmail.com>
2022-02-23 17:08:00 -0800

    commit tree da4510b3a4f2d237dbe753da74f8d7b232f2c02e

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let splitString = word.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("")
+   return (joinArray === word ? 
+   true :  false )
 }
 
 /* 
   Add your pseudocode here
+  //the function should reverse the string, then use a  === to check to see if the result matches the inputted string
 */
 
 /*
   Add written explanation of your solution here
+  // isPalindrome takes in a string. the string is then split into an array. the array of individual letters is reversed. then the reversed array is joined back together into a string. the string is now compared using a strict equals operator to the oringal inputted word. then it is put through a ternary, returning true when it is an exact match and false when it is not. 
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +30,5 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+
